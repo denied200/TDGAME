f1f6af5cc23485d4594659362b0279cb383b8ab1
Daniel Lee <danilee200@gmail.com>
2025-03-30 19:41:17 -0300

    Add files via upload

---
+++ Jogo tower defense/turret_data.py
@@ -0,0 +1,22 @@
+TURRET_DATA = [
+  {
+    #1
+    "range": 120,
+    "cooldown": 1500,
+  },
+  {
+    #2
+    "range": 150,
+    "cooldown": 1200,
+  },
+  {
+    #3
+    "range": 175,
+    "cooldown": 800,
+  },
+  {
+    #4
+    "range": 200,
+    "cooldown": 500,
+  }
+]
